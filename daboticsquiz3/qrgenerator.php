dab8b7bb87c0b0c798286fd727d85223981a3572
pratikjain2675 <163726483+pratikjain2675@users.noreply.github.com>
2024-03-17 16:24:44 +0530

    Add files via upload

---
+++ daboticsquiz3/qrgenerator.php
@@ -0,0 +1,5 @@
+<html>
+    <head>
+<title> Qr image redirector</title>
+<link rel="shortcut icon" href="images/image.png" type="image/x-icon" width="100%">
+<img src="https://chart.googleapis.com/chart?chs=300x300&cht=qr&chl=http%3A%2F%2Fwww.flipkart.com%2F&choe=UTF-8" title="Link to Google.com" />
